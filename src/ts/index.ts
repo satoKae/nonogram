038e52b42cc0bfc7bc1a153f3d850bdee432ed8f
satokae <208755468+satoKae@users.noreply.github.com>
2025-10-26 02:44:55 +0900

    feat: 初期化する盤面の大きさをinitの引数に

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,8 +1,9 @@
 import init from './init.ts';
 import '../css/global.css';
 import '../css/board.css';
+import { COLUMN_SIZE, ROW_SIZE } from './utils/consts.ts';
 
 const app = document.getElementById('app');
 if (app) {
-  init(app);
+  init(app, ROW_SIZE, COLUMN_SIZE);
 }
--- src/ts/init.ts
+++ src/ts/init.ts
@@ -1,7 +1,10 @@
-import { COLUMN_SIZE, ROW_SIZE } from './utils/consts.ts';
 import { createNewElement, TagName } from './utils/element.ts';
 
-export default function (app: HTMLElement): void {
+export default function (
+  app: HTMLElement,
+  rowSize: number,
+  columnSize: number,
+): void {
   app.innerHTML = '';
   app.appendChild(createNewElement('div', 'corner'));
   const colHintElement = app.appendChild(
@@ -15,25 +18,25 @@ export default function (app: HTMLElement): void {
   );
 
   // 横ヒント
-  for (let y = 0; y < ROW_SIZE; y++) {
+  for (let y = 0; y < rowSize; y++) {
     rowHintElement
       .appendChild(createNewElement('div', 'row-hint-cell', TagName.rowHint(y)))
       .setAttribute('data-cell-y', y.toString());
   }
 
   // 縦ヒント
-  for (let x = 0; x < COLUMN_SIZE; x++) {
+  for (let x = 0; x < columnSize; x++) {
     colHintElement
       .appendChild(createNewElement('div', 'col-hint-cell', TagName.colHint(x)))
       .setAttribute('data-cell-x', x.toString());
   }
 
   // 盤面
-  boardElement.style.setProperty('--column-size', COLUMN_SIZE.toString());
-  boardElement.style.setProperty('--row-size', ROW_SIZE.toString());
+  boardElement.style.setProperty('--column-size', columnSize.toString());
+  boardElement.style.setProperty('--row-size', rowSize.toString());
 
-  for (let y = 0; y < ROW_SIZE; y++) {
-    for (let x = 0; x < COLUMN_SIZE; x++) {
+  for (let y = 0; y < rowSize; y++) {
+    for (let x = 0; x < columnSize; x++) {
       const cellElement = boardElement.appendChild(
         createNewElement('div', 'board-cell', TagName.boardHint(x, y)),
       );
