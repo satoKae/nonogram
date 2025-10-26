7af276cda1e068f27c74d2a45d4ade8f3db7a19c
satokae <208755468+satoKae@users.noreply.github.com>
2025-10-26 16:32:43 +0900

    feat: 塗りの処理

--- src/ts/draw.ts
+++ src/ts/draw.ts
@@ -1,5 +1,8 @@
+type DrawMode = 'setPaint' | 'setBlank' | 'erase' | 'none';
+
 let isDrawing: boolean = false;
 const paintedCells = new Set<string>();
+let paintingMode: DrawMode = 'none';
 
 export function boardMouseMove(event: MouseEvent) {
   if (event.target instanceof HTMLElement) {
@@ -18,7 +21,24 @@ export function boardMouseMove(event: MouseEvent) {
     const x: number = Number(targetElement.getAttribute('data-cell-x'));
     const y: number = Number(targetElement.getAttribute('data-cell-y'));
 
-    targetElement.classList.toggle('painted');
+    switch (paintingMode) {
+      case 'setPaint':
+        if (targetElement.hasAttribute('data-status')) {
+          break;
+        }
+        targetElement.setAttribute('data-status', 'painted');
+        break;
+
+      case 'setBlank':
+        if (targetElement.hasAttribute('data-status')) {
+          break;
+        }
+        targetElement.setAttribute('data-status', 'blank');
+        break;
+
+      case 'erase':
+        targetElement.removeAttribute('data-status');
+    }
   }
 }
 
@@ -27,15 +47,26 @@ export function boardContextMenu(event: PointerEvent) {
 }
 
 export function boardMouseDown(event: MouseEvent) {
-  isDrawing = true;
-  if (event.button === 0) {
-    // 左
-  } else if (event.button === 2) {
-    // 右
+  if (event.target instanceof HTMLElement) {
+    const targetElement: HTMLElement = event.target;
+    if (!targetElement || !targetElement.classList.contains('board-cell')) {
+      return;
+    }
+
+    isDrawing = true;
+
+    if (targetElement.hasAttribute('data-status')) {
+      paintingMode = 'erase';
+    } else if (event.button === 0) {
+      paintingMode = 'setPaint';
+    } else if (event.button === 2) {
+      paintingMode = 'setBlank';
+    }
   }
 }
 
 export function documentMouseUp(event: MouseEvent) {
   isDrawing = false;
+  paintingMode = 'none';
   paintedCells.clear();
 }
