1d019193afddc39f85b009a1ea8e2bb013402385
satokae <208755468+satoKae@users.noreply.github.com>
2025-10-31 11:38:23 +0900

    feat: データからヒントの初期化

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,9 +1,18 @@
 import init from './init.ts';
 import '../css/global.css';
 import '../css/board.css';
-import { COLUMN_SIZE, ROW_SIZE } from './utils/consts.ts';
+import { BoardData } from './utils/boardData.ts';
+import { setHintData } from './utils/setBoardData.ts';
 
 const app = document.getElementById('app');
 if (app) {
-  init(app, ROW_SIZE, COLUMN_SIZE);
+  const params = new URL(document.location.toString()).searchParams;
+  const data = params.get('data');
+  try {
+    const boardData = BoardData.fromBase64String(data || '');
+    init(app, boardData.width, boardData.height);
+    setHintData(boardData);
+  } catch (e) {
+    console.error(e);
+  }
 }
